b0ed1c92c2bde914a535ea0c175f7bf605f2999f
hoco <hoco@domain.local>
2023-02-12 04:23:30 -0800

    check valid cookie,user,pass in insta bot

--- routers/instagram/instagram_views.py
+++ routers/instagram/instagram_views.py
@@ -11,29 +11,41 @@ from fastapi import APIRouter
 from fastapi import Depends
 
 router = APIRouter()
-scrap = InstagramScrapping()
 
 # save (user,pass) and cookie
-@router.post('/instagram',tags=['instagram'])
-async def add_user(instagram:Account,dependencies = Depends(get_current_user),db=Depends(get_database)) -> Cookie:
+@router.post('/instagram/login',tags=['instagram'])
+async def add_user(instagram:Account,user = Depends(get_current_user),db=Depends(get_database)):
     username = instagram.instagramID
     password = instagram.instagramPass
-    
-    await scrap.account(username, password)
-    cookie = await database.get_cookie()
-    return cookie
+    cookie = instagram.cookie
+    scrap = InstagramScrapping(db)
+    if len(cookie) > 0:
+        res = await scrap.check_cookie(**cookie)
+        if not res:
+            return {"msg":"cookie is invalid"}
+        await db.add_cookie(cookie)
+        # cookies = await database.get_cookie()
+        # return cookies
+    else:
+        res = await scrap.account(username, password)
+        if not res:
+            return {"msg":"username and password is invalid"}
+        cookies = await database.get_cookie(instaID=username)
+        return cookies
 
 # get following list
 @router.get("/instagram/following/{usr}",tags=['instagram'])
-async def list_following(username:str,dependencies = Depends(get_current_user),db=Depends(get_database)):
-    await scrap.following(username)
+async def list_following(username:str,instaID:str ,user= Depends(get_current_user),db=Depends(get_database)):
+    scrap = InstagramScrapping(db)
+    await scrap.following(username,instaID)
     res = await db.fetch_following_data(username,10)
     return serializeList(res)
     
 # get follower list
 @router.get("/instagram/follower/{usr}",tags=['instagram'])
-async def list_follower(username:str,dependencies = Depends(get_current_user),db=Depends(get_database)):
-    await scrap.follower(username)
+async def list_follower(username:str,instaID:str,user = Depends(get_current_user),db=Depends(get_database)):
+    scrap = InstagramScrapping(db)
+    await scrap.follower(username,instaID)
     res = await db.fetch_follower_data(username,10)
     return serializeList(res)
 
