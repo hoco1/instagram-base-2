ed811017bfa5098e80b1fbdfe21052520d1e023f
hoco <hoco@domain.local>
2023-02-14 06:28:23 -0800

    response,change convert list to dict

--- routers/instagram/instagram_views.py
+++ routers/instagram/instagram_views.py
@@ -10,6 +10,8 @@ from .manipulation import serializeList
 from fastapi import APIRouter
 from fastapi import Depends,HTTPException
 
+from bson.objectid import ObjectId
+
 router = APIRouter()
 
 # save (user,pass) and cookie
@@ -18,42 +20,74 @@ async def add_user(instagram:Account,user:UserPanel = Depends(get_current_user),
     username = instagram.instagramID
     password = instagram.instagramPass
     cookies = instagram.cookie
-    scrap = InstagramScrapping(db,user['username'])
+    obj = ObjectId()
+
+    userPanel = userPanel = await db.get_account(user['username']) 
+
+    scrap = InstagramScrapping(db,userPanel['username'])
     if cookies:
         if type(cookies) == list:
-            cookie = {}
-            for i in range(len(cookies)):
+            manipulatCookie = {}
+            for cookie in cookies:
                 try:
-                    key = cookies[i]['name']
-                    value = cookies[i]['value']
-                    cookie[key]=value
+                    key=cookie['name']
+                    value=cookie['value']
+                    manipulatCookie[key]=value
                 except:
                     raise HTTPException(status_code=422, detail="list format is invalid")
-            cookies = cookie
+            cookies = manipulatCookie
         res = await scrap.get_account_info(cookies)
-        print(cookie)
         print(res)
         if not res:
             raise HTTPException(status_code=401, detail="cookie is invalid")
 
-        res['userPanel']=user['username']
-        await db.add_instagram_account(res)
-        
-        cookies['userPanel']=user['username']
-        insta = res['username']
-        cookies['username']=insta
-        await db.add_cookie(cookies)
-        print("cookie is valid & insert")
+        usefulInfos = ['first_name','last_name','email','is_email_confirmed',
+                            'is_phone_confirmed','username',
+                            'phone_number','gender','birthday']
+        data = {}
+        for info in usefulInfos:
+            if res[info]:
+                    data[info]=res[info]
 
-        result = {"gender":res["gender"],
-            "username":res["username"],
-            "email":res['email'],
-            "cookie":cookie}
-        
+        data['password_instagram']=None
+        data['userPanel_id']=userPanel['_id']
+        data['cookie_id'] = obj
+
+        await db.add_instagram_account(data)
+
+        result={
+            "first_name":res['first_name'] ,
+            "last_name":res['last_name'] ,
+            "email":res['email'] ,
+            "is_email_confirmed":res['is_email_confirmed'] ,
+            "is_phone_confirmed":res['is_phone_confirmed'],
+            "username":res['username'] ,
+            "phone_number":res['phone_number'] ,
+            "gender":res['gender'] ,
+            "birthday":res['birthday'],
+            "cookie":dict(cookies)
+        }
+        cookies['_id']=obj
+
+        await db.add_cookie(cookies)
+
         return result
     if username and password:
-        result = await scrap.account(username,password)
-        return result
+        print(username,password)
+        cookie,data = await scrap.account(username,password)
+        res={
+            "first_name": data["first_name"],
+            "last_name": data.get("last_name",None),
+            "email": data["email"],
+            "is_email_confirmed": data.get("is_email_confirmed",None),
+            "is_phone_confirmed": data.get("is_phone_confirmed",None),
+            "username": data["username"],
+            "phone_number": data.get("phone_number",None),
+            "gender": data["gender"],
+            "birthday": data["birthday"],
+            "cookie":dict(cookie)
+        }
+        return res
     raise HTTPException(status_code=401, detail="invalid credentials")
 
 # get following list
